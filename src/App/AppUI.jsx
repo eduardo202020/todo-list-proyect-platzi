ceb3d2578b748f45385c4260ca75a852bfcf1597
Eduardo26 <jguevaral@uni.pe>
2022-11-06 15:57:36 -0500

    useLocalSorage

--- src/App/AppUI.jsx
+++ src/App/AppUI.jsx
@@ -5,6 +5,8 @@ import { TodoItem } from "../components/TodoItem";
 import { CreateTodoButton } from "../components/CreateTodoButton";
 
 function AppUI({
+  loading,
+  error,
   completedTodos,
   totalTodos,
   searchValue,
@@ -18,6 +20,12 @@ function AppUI({
       <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
       <TodoSearch search={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
+        {/* Mostramos un mensaje en caso de que ocurra algún error */}
+        {error && <p>Desespérate, hubo un error...</p>}
+        {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando lo sdatos */}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
+        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
