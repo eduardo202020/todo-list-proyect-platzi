0a566a2fb5f393e3dd23d144d98dd14439cc0da7
Eduardo26 <jguevaral@uni.pe>
2023-01-04 20:35:50 -0500

    Updates

---
+++ precache-manifest.82e2163a135ccf860177ca9dbe4a6d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e4bdea0eb8ca59f4af",
+    "url": "/todo-list-proyect-platzi/static/css/main.472a3e13.chunk.css"
+  },
+  {
+    "revision": "32e4bdea0eb8ca59f4af",
+    "url": "/todo-list-proyect-platzi/static/js/main.32e4bdea.chunk.js"
+  },
+  {
+    "revision": "420df3fc156659388558",
+    "url": "/todo-list-proyect-platzi/static/js/1.420df3fc.chunk.js"
+  },
+  {
+    "revision": "e316852ecbdde871620d",
+    "url": "/todo-list-proyect-platzi/static/js/runtime~main.e316852e.js"
+  },
+  {
+    "revision": "a48d93372f278051b2ae67ee8df1470d",
+    "url": "/todo-list-proyect-platzi/index.html"
+  }
+];
